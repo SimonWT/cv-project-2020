08cbe1bdbb1976045ac1fddf0f020b914784c370
Yaroslav Yudinskikh <yaskih@mail.ru>
2020-09-10 16:08:36 +0300

    add tran val .. conf

--- src/config/config.py
+++ src/config/config.py
@@ -1,3 +1,54 @@
+from yacs.config import CfgNode as CN
+
+_C = CN()
+_C.name = 'default_name'
+_C.system = CN()
+_C.system.num_gpu = 1
+_C.system.num_workers = 4
+
+_C.dataset = CN()
+_C.dataset.img_size = 512
+_C.dataset.augmentations = []
+_C.dataset.transforms = []
+
+_C.dataset.train = CN()
+_C.dataset.train.csv_path = '/home/semyon/cardiomethry/ChestXrayIndex/train_small.csv'
+_C.dataset.train.batch_size = 16
+_C.dataset.train.shuffle = True
+
+_C.dataset.val = CN()
+_C.dataset.val.csv_path = '/home/semyon/cardiomethry/ChestXrayIndex/val.csv'
+_C.dataset.val.batch_size = 16
+_C.dataset.val.shuffle = False
+
+_C.dataset.test = CN()
+_C.dataset.test.csv_path = '/home/semyon/cardiomethry/ChestXrayIndex/test.csv'
+_C.dataset.test.batch_size = 16
+_C.dataset.test.shuffle = False
+
+
+_C.model = CN()
+_C.model.name = 'Baseline'
+_C.model.in_channels = 3
+_C.model.target_points = [0]
+_C.model.final_activation = 'relu'
+_C.model.weights_imagenet = False
+_C.model.load_state = 0
+
+
+_C.training = CN()
+_C.training.num_epochs = 10
+_C.training.dump_period = 5    # period(# of epochs) of saving model state
+_C.training.log_interval = 20  # period(# of batches) to save visuals/log info
+_C.training.optimizer = [{'lr': 0.001, 'name': 'RAdam'}]
+_C.training.scheduler = [{'name': 'StepLR', 'step_size': 10}]
+
+_C.training.criterion = []
+_C.training.save_visuals = True
+
+_C.testing = CN()
+_C.testing.save_visuals = True
+
 def __get_cfg_defaults():
   """Get a yacs CfgNode object with default values for my_project."""
   # Return a clone so that the defaults will not be altered
